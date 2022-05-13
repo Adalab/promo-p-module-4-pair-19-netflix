938161f8a0beae8a8801dd6cbdb50fa521b23afe
miranda-zapata <miranda.zapata@gmail.com>
2022-05-13 11:27:51 +0200

    Add endpoint (4.2. ex 1)

--- src/index.js
+++ src/index.js
@@ -1,13 +1,39 @@
 const express = require('express');
 const cors = require('cors');
+const movies = require('./data/movies.json');
 
-// create and config server
+// Create and config server
 const server = express();
 server.use(cors());
 server.use(express.json());
 
-// init express aplication
+// Init express aplication
 const serverPort = 4000;
 server.listen(serverPort, () => {
   console.log(`Server listening at http://localhost:${serverPort}`);
+});
+
+// Endpoint para obtener las películas
+server.get('/movies', (req, res) => {
+  const response = {
+    success: true,
+    // 13 de mayo FIN
+    movies: [
+      {
+        id: '1',
+        title: 'Gambito de dama',
+        gender: 'Drama',
+        image:
+          '//beta.adalab.es/curso-intensivo-fullstack-recursos/apis/netflix-v1/images/gambito-de-dama.jpg',
+      },
+      {
+        id: '2',
+        title: 'Friends',
+        gender: 'Comedia',
+        image:
+          '//beta.adalab.es/curso-intensivo-fullstack-recursos/apis/netflix-v1/images/friends.jpg',
+      },
+    ],
+  };
+  res.json(response);
 });
