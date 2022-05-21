210cbfbb9bb6e6829688b1cac597ca418482a0e3
miranda-zapata <miranda.zapata@gmail.com>
2022-05-21 11:17:31 +0200

    Frontend user profile update (4.6 exercise 4)

--- web/src/services/api-user.js
+++ web/src/services/api-user.js
@@ -30,7 +30,6 @@ const sendSingUpToApi = (data) => {
     userEmail: data.email,
     userPass: data.password,
   };
-  // CAMBIA ESTE FETCH PARA QUE APUNTE A UN ENDPOINT DE TU SERVIDOR, PIENSA SI DEBE SER GET O POST, PIENSA QUÉ DATOS DEBES ENVIAR, ETC
   return fetch('http://localhost:4001/sign-up', {
     // Método POST
     method: 'POST',
@@ -42,7 +41,6 @@ const sendSingUpToApi = (data) => {
   })
     .then((response) => response.json())
     .then((data) => {
-      // CAMBIA EL CONTENIDO DE ESTE THEN PARA GESTIONAR LA RESPUESTA DEL SERVIDOR Y RETORNAR AL COMPONENTE APP LO QUE NECESITA
       return data;
     });
 };
@@ -50,10 +48,22 @@ const sendSingUpToApi = (data) => {
 // Profile
 const sendProfileToApi = (userId, data) => {
   console.log('Se están enviando datos al profile:', userId, data);
-  // CAMBIA ESTE FETCH PARA QUE APUNTE A UN ENDPOINT DE TU SERVIDOR, PIENSA SI DEBE SER GET O POST, PIENSA QUÉ DATOS DEBES ENVIAR, ETC
-  return fetch(
-    '//beta.adalab.es/curso-intensivo-fullstack-recursos/apis/netflix-v1/empty.json'
-  );
+  const bodyParams = {
+    userEmail: data.email,
+    userPass: data.password,
+  };
+  return fetch('http://localhost:4001/user/profile', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      userId: userId,
+    },
+    body: JSON.stringify(bodyParams),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      return data;
+    });
 };
 
 const getProfileFromApi = (userId) => {
